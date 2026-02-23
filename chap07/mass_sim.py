861a8f689de8a54c4d243cb6768d43c64127af61
Ben-Aguero <agueroben11@gmail.com>
2026-02-23 16:39:05 -0700

    Updated

--- chap07/mass_sim.py
+++ chap07/mass_sim.py
@@ -7,14 +7,14 @@ from case_studies import common, D_mass
 
 mass = D_mass.Dynamics()
 controller = D_mass.ControllerPD(use_feedback_linearization=True)
-z_ref = common.SignalGenerator(amplitude=1.0, frequency=0.05)
+z_ref = common.SignalGenerator(amplitude=1.0, frequency=0.01)
 
 time, x_hist, u_hist, r_hist, *_ = common.run_simulation(
     mass,
     [z_ref],
     controller,
     controller_input="state",
-    t_final=5,
+    t_final=10,
     dt=D_mass.params.ts,
 )
 
--- src/case_studies/D_mass/pd_controller.py
+++ src/case_studies/D_mass/pd_controller.py
@@ -2,79 +2,53 @@
 import numpy as np
 
 # local (controlbook)
-from case_studies import common
-import case_studies.D_mass.params as P
+from .. import common
+from . import params as P
+from ..control.pd import PD
+from ..control import utils_design
 
 
 class MassControllerPD(common.ControllerBase):
-    def __init__(self, use_feedback_linearization: bool = True):
-        # tuning parameters
-        p1 = -1
-        p2 = -1.5
-
-        # For part D.8a-----------------------
-        # tr = 2.0
-        # zeta = .707
-        # wn = 2.2/tr
-        # alpha1 = wn**2
-        # alpha0 = 2*zeta*wn
-        #--------------------------------------
-
-        # For part D.8b-----------------------
-        # F_max = P.force_max
-        # m = P.m
-        # k = P.k
-        # zeta = 0.707
-        # wn_saturated = np.sqrt((F_max + k) / m)
-        # wn = wn_saturated
-        # alpha1 = wn**2
-        # alpha0 = 2 * zeta * wn
-        #-------------------------------------
-
+    def __init__(self, chapter8=False):
         # system parameters
         b0 = P.tf_num[-1]
         a1, a0 = P.tf_den[-2:]
-
-        # desired characteristic equation parameters
-        # des_CE: s^2 + alpha1*s + alpha0 = s^2 + (a1 + b0*kd)s + (a0 + b0*kp)
-        # FOR D.7 ----------------------------------------------
-        des_CE = np.poly([p1, p2])
-        alpha1, alpha0 = des_CE[-2:]
-        #-----------------------------------------------------
-
+        if chapter8:
+            # tuning parameters
+            tr = 2.0 # original
+            tr = 1.65 # tuned to barely saturate on first step only
+            zeta = 0.707
+            # desired characteristic equation parameters
+            wn = utils_design.get_natural_frequency(tr, zeta)
+            alpha1 = 2.0 * zeta * wn
+            alpha0 = wn**2
+        else: # chapter 7
+            # tuning parameters
+            p1 = -1.0
+            p2 = -1.5
+            # desired characteristic equation parameters
+            # des_CE: s^2 + alpha1*s + alpha0 = s^2 + (a1 + b0*kd)s + (a0 + b0*kp)
+            des_CE = np.poly([p1, p2])
+            alpha1, alpha0 = des_CE[-2:]
         # find gains
-        self.kp = (alpha0 - a0) / b0
-        self.kd = (alpha1 - a1) / b0
-        # self.kp = 3.05
-        # self.kd = 7.275
-        print(f"{self.kp = :.2f}, {self.kd = :.3f}")
+        kp = (alpha0 - a0) / b0
+        kd = (alpha1 - a1) / b0
+        print(f"{kp = :.2f}, {kd = :.2f}")
 
-        # For part D.8b ------------------------------------------
-        # self.limit = F_max
-        # -------------------------------------------------------
-
-        self.F_eq = P.u_eq
-        self.use_feedback_linearization = use_feedback_linearization
+        self.z_pd = PD(kp, kd)
+        self.F_eq = 0.0
+        self.chapter8 = chapter8
 
     def update_with_state(self, r, x):
         # unpack references and states
         z_ref = r[0]
-        z, zdot = x
+        z = x[0]
+        zdot = x[1]
 
         # theta (modified) PD
-        error = z_ref - z
-        F_tilde = self.kp * error - self.kd * zdot
+        F_tilde = self.z_pd.update_modified(z_ref, z, zdot)
+        u = np.array([F_tilde + self.F_eq])
 
-        if self.use_feedback_linearization:
-            F_fl = 0
-            F = F_tilde + F_fl
-        else:  # use equilibrium (Jacobian linearization)
-            # NOTE: likely not work well (or at all) for large angles
-            F = F_tilde + self.F_eq
-
-        # --- SATURATION (Required for Part B) ---
-        # u_unsat = np.array([F])
-        # u = self.saturate(u_unsat, self.limit)
-
-        u = u = np.array([F])
+        if self.chapter8: # don't change results from chapter 7 PD
+            u = self.saturate(u, u_max=P.force_max)
         return u
